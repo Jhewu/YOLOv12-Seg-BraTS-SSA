11abf847b604f3b3abd861183faa45cce576ce26
jhewu <junyi.hewu2003@gmail.com>
2026-01-02 19:38:38 -0500

    integrated a profiler for run_yolo

--- parameters.py
+++ parameters.py
@@ -5,16 +5,16 @@ https://docs.ultralytics.com/usage/cfg/#tasks
 for more information
 """
 
-### General parameters
-MODE = "train"            # train, val, test, predict 
-MODEL = "yolo12n"
+# General parameters
+MODE = "train"            # train, val, test, predict
+MODEL = "yolo12s-seg"
 DATASET = "data/data.yaml"
 SEED = 42
 
-### Training parameters
+# Training parameters
 PRETRAINED = False
 RESUME = False
-EPOCH = 75
+EPOCH = 100
 BATCH = 128
 IMAGE_SIZE = 160
 CLOSE_MOSAIC = 0
@@ -33,36 +33,26 @@ PLOT = True
 FREEZE = None
 
 # Loss Weights
-CLS=0.5 
-BOX=6       # 6.5 # 7.5 
-DFL=3.5     # 2.5 # 1.5
+CLS = 0.5
+BOX = 6       # 6.5 # 7.5
+DFL = 3.5     # 2.5 # 1.5
 
-### Augmentation parameters 
+# Augmentation parameters
 HSV_H = 0.0
 HSV_S = 0.0
 MOSAIC = 0.0
 
 HSV_V = 0.0         # 0.25, previously, but with 4-channels, it does not work anymore
-TRANSLATE = 0 # 0.1
-SCALE = 0 # 0.25
-FLIPUD = 0 # 0.5
-FLIPLR = 0 #0.5
+TRANSLATE = 0.05  # 0.1
+SCALE = 0.1  # 0.25
+FLIPUD = 0  # 0.5
+FLIPLR = 0.5  # 0.5
 
-DEGREES = 0 # 0.1       # 2.5 use it carefully
-SHEAR =  0 #1         # 5 use it carefully
-PERSPECTIVE = 0 #0.001 # 0.010 use it carefully
+DEGREES = 10  # 0.1       # 2.5 use it carefully
+SHEAR = 0  # 1         # 5 use it carefully
+PERSPECTIVE = 0  # 0.001 # 0.010 use it carefully
 MIXUP = 0.0         # 0.5 maybe a good and also bad idea
-CUTMIX = 0.0        # maybe 
+CUTMIX = 0.0        # maybe
 
 LOAD_AND_TRAIN = False
-BEST_MODEL_DIR_TRAIN = "train_yolo12x-seg_2025_11_14_17_45_37/yolo12x-seg_data/data.yaml/weights/best.pt"
-
-### Validation
-BEST_MODEL_DIR_VAL = ""
-
-### Testing
-BEST_MODEL_DIR_TEST = ""
-
-### Predict
-BEST_MODEL_DIR_PREDICT = ""
-IMAGE_TO_TEST = ""
+BEST_MODEL_DIR = "/home/jun/Desktop/inspirit/YOLOv12-Seg-BraTS-SSA/2026/yolo12n_det_aug/yolo12n_data/weights/best.pt"
--- run_yolo.py
+++ run_yolo.py
@@ -1,118 +1,198 @@
+# Local
 from custom_yolo_trainer.custom_trainer import CustomSegmentationTrainer, CustomDetectionTrainer
 from custom_yolo_predictor.custom_detseg_predictor import CustomSegmentationPredictor
 from parameters import *
 
+# Internal
 import time
 import os
 
-# os.environ["OMP_NUM_THREADS"] = "4"
+# External
+import torch
+from torch.profiler import profile, ProfilerActivity, record_function
 
-def get_current_time() -> str: 
+
+def get_current_time() -> str:
     """
-    Returns: 
+    Returns:
         (str): time in YmdHMS format
     """
     current_time = time.localtime()
     return time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
 
+
 def create_dir(folder_name: str) -> None:
     """
     Creates the given directory if it does not exist
-    Args: 
+    Args:
         folder_name (str): directory to create
     """
     if not os.path.exists(folder_name):
-        os.makedirs(folder_name)  
+        os.makedirs(folder_name)
 
-def train_yolo() -> None: 
+
+def test_yolo(warm_ups: bool = True,
+              batch: int = 128,
+              iterations: int = 3) -> None:
+    # Set environment variables to restrict other libraries to 1 thread
+    os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    os.environ["MKL_NUM_THREADS"] = "1"
+    os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+    os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+    # Restrict PyTorch's intra-op parallelism to 1 thread
+    torch.set_num_threads(1)
+
+    # You can also check the current setting
+    print(f"PyTorch using {torch.get_num_threads()} threads.")
+
+#     args = dict(
+#         # General Hyperparameters
+#         model=f"{MODEL}.yaml",
+#         data=DATASET,
+#         epochs=EPOCH,
+#         pretrained=PRETRAINED,
+#         imgsz=IMAGE_SIZE,
+#         single_cls=SINGLE_CLS,
+#         close_mosaic=CLOSE_MOSAIC,
+#         fraction=FRACTION,
+#         freeze=None,
+#         lr0=INITIAL_LR,
+#         lrf=FINAL_LR,
+#         warmup_epochs=WARMUP_EPOCH,
+#         cls=CLS,
+#         box=BOX,
+#         dfl=DFL,
+#         seed=SEED,
+#         batch=BATCH,
+#         amp=MIX_PRECISION,
+#         multi_scale=MULTI_SCALE,
+#         cos_lr=COS_LR,
+#         plots=PLOT,
+#         profile=PROFILE,
+#         project=f"{MODE}_{MODEL}_{get_current_time()}",
+#         name=f"{MODEL}_{DATASET}",
+# 
+#         # Data Augmentation Hyperparameters
+#         hsv_h=HSV_H,
+#         hsv_s=HSV_S,
+#         hsv_v=HSV_V,
+#         degrees=DEGREES,
+#         translate=TRANSLATE,
+#         scale=SCALE,
+#         flipud=FLIPUD,
+#         fliplr=FLIPLR,
+#         mosaic=MOSAIC,
+#         shear=SHEAR,
+#         perspective=PERSPECTIVE,
+#         mixup=MIXUP,
+#         cutmix=CUTMIX)
+# 
+#     if LOAD_AND_TRAIN:
+#         print("\nLoading and Training...")
+#         args["model"] = BEST_MODEL_DIR
+#         args["resume"] = RESUME
+# 
+#     YOLO_predictor = CustomSegmentationPredictor(overrides=args)
+#     YOLO_predictor.setup_model(args["model"])
+#     model = YOLO_predictor.model.model
+# 
+#     model.to("cpu")
+#     model.eval()
+# 
+#     dummy_data = torch.zeros(batch, 4, 160, 160)
+
+    from ultralytics import YOLO
+
+    model = YOLO("yolov12x-seg.pt")
+
+
+    if warm_ups:
+        for _ in range(2):
+            model(dummy_data)
+        with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+            with record_function("model_inference"):
+                for _ in range(iterations):
+                    model(dummy_data)
+
+        # Get the key averages
+        key_avg = prof.key_averages()
+
+        # Calculate total CPU time from all operations
+        total_cpu_time = sum([item.self_cpu_time_total for item in key_avg])
+        avg_time_per_iteration = total_cpu_time / iterations
+
+        print(f"--- Results averaged over {iterations} iterations ---")
+        print(f"Total CPU time: {total_cpu_time / 1e6:.2f}")
+        print(f"Average time per iteration: {
+              avg_time_per_iteration / 1e6:.2f}")
+        print("\nPer-operation breakdown:")
+        print(prof.key_averages().table(
+            sort_by="cpu_time_total",
+            row_limit=10
+        ))
+
+
+def train_yolo() -> None:
     """
     Trains YOLO model. All hyperparameters are configured in parameters.py
     """
-    print("\nStarting Training...")
 
     print(f"\nThis is dataset {f"./data/{DATASET}.yaml"}\n")
 
     args = dict(
-                # General Hyperparameters
-                model=f"{MODEL}.yaml", 
-                data=DATASET, 
-                epochs=EPOCH, 
-                pretrained=PRETRAINED, 
-                imgsz=IMAGE_SIZE, 
-                single_cls=SINGLE_CLS, 
-                close_mosaic=CLOSE_MOSAIC, 
-                fraction=FRACTION,
-                freeze=None,  
-                lr0=INITIAL_LR, 
-                lrf=FINAL_LR, 
-                warmup_epochs=WARMUP_EPOCH, 
-                cls=CLS, 
-                box=BOX, 
-                dfl=DFL, 
-                seed=SEED, 
-                batch=BATCH,
-                amp=MIX_PRECISION, 
-                multi_scale=MULTI_SCALE, 
-                cos_lr=COS_LR,
-                plots=PLOT,
-                profile=PROFILE,
-                project=f"{MODE}_{MODEL}_{get_current_time()}",
-                name=f"{MODEL}_{DATASET}", 
-                
-                # Data Augmentation Hyperparameters
-                hsv_h=HSV_H, 
-                hsv_s=HSV_S, 
-                hsv_v=HSV_V, 
-                degrees=DEGREES,
-                translate=TRANSLATE,
-                scale=SCALE,
-                flipud=FLIPUD, 
-                fliplr=FLIPLR, 
-                mosaic=MOSAIC, 
-                shear=SHEAR, 
-                perspective=PERSPECTIVE, 
-                mixup=MIXUP, 
-                cutmix=CUTMIX)
-    
-    if LOAD_AND_TRAIN: 
+        # General Hyperparameters
+        model=f"{MODEL}.yaml",
+        data=DATASET,
+        epochs=EPOCH,
+        pretrained=PRETRAINED,
+        imgsz=IMAGE_SIZE,
+        single_cls=SINGLE_CLS,
+        close_mosaic=CLOSE_MOSAIC,
+        fraction=FRACTION,
+        freeze=None,
+        lr0=INITIAL_LR,
+        lrf=FINAL_LR,
+        warmup_epochs=WARMUP_EPOCH,
+        cls=CLS,
+        box=BOX,
+        dfl=DFL,
+        seed=SEED,
+        batch=BATCH,
+        amp=MIX_PRECISION,
+        multi_scale=MULTI_SCALE,
+        cos_lr=COS_LR,
+        plots=PLOT,
+        profile=PROFILE,
+        project=f"{MODE}_{MODEL}_{get_current_time()}",
+        name=f"{MODEL}_{DATASET}",
+
+        # Data Augmentation Hyperparameters
+        hsv_h=HSV_H,
+        hsv_s=HSV_S,
+        hsv_v=HSV_V,
+        degrees=DEGREES,
+        translate=TRANSLATE,
+        scale=SCALE,
+        flipud=FLIPUD,
+        fliplr=FLIPLR,
+        mosaic=MOSAIC,
+        shear=SHEAR,
+        perspective=PERSPECTIVE,
+        mixup=MIXUP,
+        cutmix=CUTMIX)
+
+    if LOAD_AND_TRAIN:
         print("\nLoading and Training...")
-        args["model"] = BEST_MODEL_DIR_TRAIN
+        args["model"] = BEST_MODEL_DIR
         args["resume"] = RESUME
-    
-    # trainer = CustomSegmentationTrainer(overrides=args)
-    trainer = CustomDetectionTrainer(overrides=args)
+
+    trainer = CustomSegmentationTrainer(overrides=args)
+    # trainer = CustomDetectionTrainer(overrides=args)
     trainer.train()
 
 
-#     p_args = dict(model="yolo12n-seg_converged/weights/best.pt",
-#             data=f"data/data.yaml", 
-#             device="cpu", 
-#             verbose=True,
-#             imgsz=160, 
-#             save=False)
-#             
-#     YOLO_predictor = CustomSegmentationPredictor(overrides=p_args)
-#     YOLO_predictor.setup_model(p_args["model"])
-# 
-#     import cv2
-#     from torchvision import transforms
-# 
-#     x = cv2.imread("BraTS-SSA-00046-00071-t1c.png", cv2.IMREAD_UNCHANGED)
-#     x = transforms.ToTensor()(x)
-#     x = transforms.Resize(size=(160, 160))(x)
-#     x = x.to("cpu")  
-#     x = x.unsqueeze(0)
-#     # x = x.repeat(155, 1, 1, 1)
-# 
-#     # YOLO_predictor(x)
-#     import time 
-#     start = time.perf_counter()
-#     YOLO_predictor.model.model(x)
-#     end = time.perf_counter()
-#     print((end - start)*1000)
-
-    # print(f"\nEnsuring the Model's input layer was changed: {trainer.setup_model()}")
-    # print(f"\nFinish training, please check your directory for folder named 'train-....")
-
 if __name__ == "__main__":
     train_yolo()
+    # test_yolo()
